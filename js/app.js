61dcad4fecc07d627adcb593a9d9bdb339701096
Jason Jones <jkjmr6@gmail.com>
2015-06-11 11:33:14 -0500

    Added foundation js snippet

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 jQuery(document).ready(function($){ 
+	//Include nav logic
 	$( ".nav-toggle" ).click(function() {
 	  $(this).toggleClass("open");
 	  $("nav").fadeToggle(100);
 
 	  return false;
 	});
+
+	//Fondation
+	$(document).foundation();
 });		
