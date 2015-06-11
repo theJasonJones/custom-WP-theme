adefce9430de5bfd7745a45db1ea6360fbf2ce73
Jason Jones <jkjmr6@gmail.com>
2015-06-11 11:31:43 -0500

    Added wp_footer()

--- footer.php
+++ footer.php
@@ -1,1 +1,12 @@
-<p>Footer</p>
+    <div class="footer-clear"></div>
+      <footer class="row no-max pad">          
+        <p>Copyright <?php echo date('Y'); ?></p>
+      </footer>
+
+      <?php 
+      //Output footer files
+      wp_footer(); 
+      ?> 
+
+    </body>
+</html>
