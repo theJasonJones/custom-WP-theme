2efdce95c7e87c2bb8461a55a1ae3b5d9275f539
Jason Jones <jkjmr6@gmail.com>
2015-06-12 12:46:05 -0500

    Added comments to single page

--- single.php
+++ single.php
@@ -10,10 +10,13 @@
 			      <?php if( have_posts() ) : while ( have_posts() ) : the_post(); ?>
  				<article class="post">
 	                <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-	                <p><?php the_excerpt(); ?></p>
+	                <?php if( get_the_post_thumbnail() ): ?>
+	                <div class="img-container">
+	                  <?php the_post_thumbnail('large'); ?>
+	                </div>
+	            	<?php endif; ?>
 	                <ul class="post-meta no-bullet">
 	                  <li class="author">
-	                    <a href="author.html">
 	                      <span class="wpt-avatar small">
 	                      	<?php echo get_avatar( get_the_author_meta( 'ID' ), 24 ); ?>
 	                      </span>
@@ -23,12 +26,9 @@
 	                  <li class="cat">in <?php the_category(', '); ?></li>
 	                  <li class="date"> on <?php the_time('F j, Y'); ?></li> <!-- use the_time instead of the_date to prevent some dates note appearing-->
 	                </ul>
-	                <?php if( get_the_post_thumbnail() ): ?>
-	                <div class="img-container">
-	                  <img src="<?php the_post_thumbnail('large'); ?>" alt="">
-	                </div>
-	            	<?php endif; ?>
-                </article>
+	                <?php the_content() ?>
+	                <?php comments_template(); ?>
+	            </article>
 			      <?php endwhile; else: ?>
 
 			      	<p><?php _e('Sorry, no posts here.'); ?></p>
