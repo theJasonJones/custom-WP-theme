eb0aa67b0b3ef1230ac2fe4bec6d4bf8bd148e7a
Jason Jones <jkjmr6@gmail.com>
2015-06-12 12:27:07 -0500

    Added single for posts

---
+++ single.php
@@ -0,0 +1,49 @@
+<?php get_header(); ?>
+
+ <section class="two-column row no-max pad">
+  	<div class="small-12 columns">
+    	<div class="row">
+      	<!-- Primary Column -->
+      		<div class="small-12 medium-7 medium-offset-1 medium-push-4 columns">
+        		<div class="primary">
+
+			      <?php if( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+ 				<article class="post">
+	                <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+	                <p><?php the_excerpt(); ?></p>
+	                <ul class="post-meta no-bullet">
+	                  <li class="author">
+	                    <a href="author.html">
+	                      <span class="wpt-avatar small">
+	                      	<?php echo get_avatar( get_the_author_meta( 'ID' ), 24 ); ?>
+	                      </span>
+	                      by <?php the_author_posts_link(); ?>
+	                    </a>
+	                  </li>
+	                  <li class="cat">in <?php the_category(', '); ?></li>
+	                  <li class="date"> on <?php the_time('F j, Y'); ?></li> <!-- use the_time instead of the_date to prevent some dates note appearing-->
+	                </ul>
+	                <?php if( get_the_post_thumbnail() ): ?>
+	                <div class="img-container">
+	                  <img src="<?php the_post_thumbnail('large'); ?>" alt="">
+	                </div>
+	            	<?php endif; ?>
+                </article>
+			      <?php endwhile; else: ?>
+
+			      	<p><?php _e('Sorry, no posts here.'); ?></p>
+
+			      <?php endif; ?>
+
+			    </div>
+	  		</div>
+	    </div>
+    </div>
+<!-- Secondary Column -->
+	<div class="small-12 medium-4 medium-pull-8 columns">
+	    <div class="secondary">
+	      <h2 class="module-heading">Sidebar</h2>
+	    </div>
+	</div>
+</section>
+<?php get_footer(); ?>
